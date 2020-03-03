515f1f67ee4470888bb91e74bd59d410c3e81515
Steve Karsch <steve@stevekarsch.com>
2020-03-03 12:11:59 -0500

    SISA-0026 Update gulpfile to minify css. Update CSS to minified versions.

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var csso = require('gulp-csso');
 
 gulp.task(
   'styles',
   function() {
     return gulp.src('docroot/assets/sass/**/*.scss')
       .pipe(sass().on('error', sass.logError))
+      .pipe(csso())
       .pipe(gulp.dest('docroot/assets/css/'));
   }
 );
